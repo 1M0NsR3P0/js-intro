bdf6772cedb19aa25cb341411e2a017fccfff5f2
imon <naimonchy@gmail.com>
2023-01-28 21:24:00 +0600

    more js added

---
+++ practice-day/ajkamarmonvalonei.js
@@ -0,0 +1,22 @@
+let string = 1;
+let num = 58;
+let odd;
+let even;
+while(string <= 39 || num <=98){
+    if(string == 5 || string == 10 || string == 15 )
+    {
+        console.log(`ajk amar mon valo ace... colo berate jai...`);
+        string++;
+        continue;
+    }
+    console.log(`ajk amar mon valo nei : `+string)
+    console.log(`\n`+num)
+    string++;
+    num++;
+}
+for(odd == 581 || even == 412; odd <= 623 || even == 546; odd = odd+2)
+{
+    even =even+2;
+    console.log(odd + `\n`);
+    console.log(even)
+}
