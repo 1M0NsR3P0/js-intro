3ef2ac62ef55c4066e12cc162c9e4c07b1440ac7
imon <naimonchy@gmail.com>
2023-01-28 22:57:18 +0600

    added more math method

--- Math/math.tranc().js
+++ Math/math.tranc().js
@@ -2,3 +2,4 @@ console.log(Math.trunc(5.22))
 console.log(Math.trunc(246.2153453))
 console.log(Math.trunc(0.56456))
 console.log(Math.trunc(75.21121547))
+// it just give the exact value of a number and return an absolute value
